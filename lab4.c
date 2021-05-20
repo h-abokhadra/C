60f361dd4f6f595413de5a33c9e8728e39c1ddce
h-abokhadra <59623466+h-abokhadra@users.noreply.github.com>
2021-05-20 10:12:19 -0400

    Lab4

---
+++ lab4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/*4th lab*/
+
+int main(int argc, char** argv){
+	
+	int x, y; //declaring variables
+	x = 5;
+	y = -5;
+	
+	printf("The value of x is %d", x);
+	printf("\nThe value of y is %d", y);
+    printf("\nThe value of x is %d and value of y is %d", x, y);
+
+	return 0;
+}
