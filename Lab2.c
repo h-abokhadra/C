65ee06af637377098aa570f1d956d8cee641eb9e
h-abokhadra <59623466+h-abokhadra@users.noreply.github.com>
2021-05-20 10:03:23 -0400

    Lab 2

---
+++ Lab2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/*2nd lab*/
+
+int main(int argc, char** argv){
+	
+	printf("\'My name is Hoda Abokhadra\'");
+    printf("\"My name is Hoda Abokhadra\"");
+    printf("My name is \t\t\t Hoda Abokhadra");
+
+	return 0;
+}
