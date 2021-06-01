d32ed1c706e1a587a1564f375aa4ff9c09d984d5
h-abokhadra <59623466+h-abokhadra@users.noreply.github.com>
2021-06-01 13:43:59 -0400

    UPDATE

---
+++ Week3A/Exercise1.c
@@ -0,0 +1,13 @@
+// Program to find the datatypes
+#include <stdio.h>
+int main(int argc, char** argv)
+{
+	printf("\nThe size of integer is %d", sizeof(int)); 
+	printf("\nThe size of short integer is %d", sizeof(short int));
+	printf("\nThe size of unsigned integer is %d", sizeof(unsigned int));
+	printf("\nThe size of signed integer is %d", sizeof(signed int));
+	printf("\nThe size of long integer is %d", sizeof(long int));
+	printf("\nThe size of character is %d", sizeof(char));
+	printf("\nThe size of floating number is %d", sizeof(float)); 
+	printf("\nThe size of double number is %d", sizeof(double)); 
+}
