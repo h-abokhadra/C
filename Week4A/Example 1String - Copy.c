c15d835e556b7e1c8c5c5e5e1001a9209fee91c9
h-abokhadra <59623466+h-abokhadra@users.noreply.github.com>
2021-06-20 22:30:34 -0400

    TESTING

---
+++ Week4A/Example 1String - Copy.c
@@ -0,0 +1,14 @@
+// C program to illustrate strings 
+
+#include<stdio.h> 
+
+int main(int argc, char** argv)
+{  
+ 	       // prints             
+	  
+	 	char str2[10];
+		str2 = "HELLO";         // prints 
+	 	printf ("%s",str2); 
+	    
+} 
+
